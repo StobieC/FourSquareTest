d02f880a871e7bb137c60122022004a16fe524aa
StobieC <stobiecameron@gmail.com>
2016-02-28 17:11:52 +0000

    Volley setup

--- app/src/main/java/com/example/cameron/foursquaretest/MainActivity.java
+++ app/src/main/java/com/example/cameron/foursquaretest/MainActivity.java
@@ -4,12 +4,21 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText search;
+    RecyclerView recyclerView;
+    Button searchBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +27,12 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        search = (EditText) findViewById(R.id.et_search);
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(MainActivity.this, LinearLayoutManager.VERTICAL, false);
+
+        recyclerView.setLayoutManager(layoutManager);
+        searchBtn = (Button) findViewById(R.id.button);
     }
 
     @Override
---
+++ app/src/main/java/com/example/cameron/foursquaretest/Model/Constants.java
@@ -0,0 +1,12 @@
+package com.example.cameron.foursquaretest.Model;
+
+/**
+ * Created by Cameron on 28/02/2016.
+ */
+public class Constants {
+
+    public static String CLIENT_ID = "client_id=R0LB4G2EP5ZKUE3NJ4MNP4EF4SI5SPO0M4OQ0WLKAQRD5ZPF";
+    public static String CLIENT_SECRET = "client_secret=NNDKKUNAX4ZJ1HL0ODQDSIJ35KFDB3FFBTYCRWFR1G5PQT0T";
+    public static String URL = "https://api.foursquare.com/v2/venues/search?" + CLIENT_ID + CLIENT_SECRET ;
+
+}
