68971f08614c1c33b24a88944a173b01644f955a
StobieC <stobiecameron@gmail.com>
2016-02-28 17:50:29 +0000

    make JSON request

--- app/src/main/java/com/example/cameron/foursquaretest/MainActivity.java
+++ app/src/main/java/com/example/cameron/foursquaretest/MainActivity.java
@@ -7,11 +7,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.example.cameron.foursquaretest.Model.Constants;
+import com.example.cameron.foursquaretest.adapter.MyAdapter;
+import com.example.cameron.foursquaretest.app.AppController;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,6 +48,43 @@ public class MainActivity extends AppCompatActivity {
 
         recyclerView.setLayoutManager(layoutManager);
         searchBtn = (Button) findViewById(R.id.button);
+
+
+        searchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String searchRequest = search.getText().toString();
+                makeRequestJSON(searchRequest);
+            }
+        });
+    }
+
+    void makeRequestJSON(String searchtxt)
+    {
+        Log.e("search for:", searchtxt);
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET,
+                Constants.URL + searchtxt, null,
+                new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.e("TAG RESPONSE", response.toString());
+                        //TODO convert response
+                        MyAdapter rvAdapter = new MyAdapter(/* list */ , MainActivity.this);
+                        recyclerView.setAdapter(rvAdapter);
+
+                    }
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d("TAG", "Error: " + error.getMessage());
+            }
+        });
+
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(jsonObjReq);
+
     }
 
     @Override
